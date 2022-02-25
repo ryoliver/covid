1f7cebc8697be53a3d35a0511b89976eddb2dd1d
Ruth Oliver <ruth.oliver@yale.edu>
2022-02-25 13:01:22 -0800

    update workflow

--- src/workflow/workflow_farnam.sh
+++ src/workflow/workflow_farnam.sh
@@ -9,6 +9,7 @@ chmod +x $src/workflow/run_safegraph_processing.sh
 chmod +x $src/workflow/run_cbg_intersection.sh
 chmod +x $src/workflow/run_safegraph_annotation.sh
 chmod +x $src/workflow/run_census_annotation.sh
+chmod +x $src/workflow/run_ghm_annotation.sh
 chmod +x $src/workflow/run_event_summary.sh
 chmod +x $src/workflow/run_dbbmm_test.sh
 
@@ -26,8 +27,11 @@ chmod +x $src/workflow/run_dbbmm_test.sh
 # annotate events with census data
 #sbatch $src/workflow/run_census_annotation.sh
 
+# annotate events with census data
+#sbatch $src/workflow/run_ghm_annotation.sh
+
 # summarize event data
 #sbatch $src/workflow/run_event_summary.sh
 
 # summarize event data
-sbatch $src/workflow/run_dbbmm_test.sh
+#sbatch $src/workflow/run_dbbmm_test.sh
