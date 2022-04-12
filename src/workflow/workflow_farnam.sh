e620797a5b9852159ea2b7fa15de6f3e1f6118ad
Ruth Oliver <ruth.oliver@yale.edu>
2022-04-12 10:44:31 -0700

    extract ghm from cbg

--- src/workflow/workflow_farnam.sh
+++ src/workflow/workflow_farnam.sh
@@ -7,27 +7,29 @@ src=/gpfs/ysm/project/jetz/ryo3/projects/covid/src
 #make executable
 chmod +x $src/workflow/create_intersection_joblist.R
 chmod +x $src/workflow/run_safegraph_processing.sh
-chmod +x $src/workflow/run_cbg_intersection.sh
+#chmod +x $src/workflow/run_cbg_intersection.sh
 chmod +x $src/workflow/run_safegraph_annotation.sh
 chmod +x $src/workflow/run_census_annotation.sh
 chmod +x $src/workflow/run_ghm_annotation.sh
 chmod +x $src/workflow/run_event_summary.sh
 chmod +x $src/workflow/run_dbbmm_test.sh
+chmod +x $src/workflow/run_extract_gHM.sh
 
-#run
+# intersect events with census geometries
+#module load R/4.1.0-foss-2020b
+#Rscript $src/workflow/create_intersection_joblist.R
 
-module load R/4.1.0-foss-2020b
-Rscript $src/workflow/create_intersection_joblist.R
+#module load dSQ
+#dsq --job-file $src/workflow/joblist.txt --mem-per-cpu 40g -t 2- 
 
-module load dSQ
-dsq --job-file $src/workflow/joblist.txt --mem-per-cpu 40g -t 2- 
-
-sbatch dsq-joblist-2022-04-11.sh
+# UPDATE WITH DATE
+#sbatch dsq-joblist-2022-04-11.sh
 
 # process safegraph data
 #sbatch $src/workflow/run_safegraph_processing.sh
 
 # intersect events with census geometries
+# deprecated?
 #sbatch $src/workflow/run_cbg_intersection.sh
 
 # annotate events with safegraph data
@@ -43,4 +45,8 @@ sbatch dsq-joblist-2022-04-11.sh
 #sbatch $src/workflow/run_event_summary.sh
 
 # summarize event data
+# deprecated?
 #sbatch $src/workflow/run_dbbmm_test.sh
+
+# extract gHM from census geometries
+sbatch $src/workflow/run_extract_gHM.sh
