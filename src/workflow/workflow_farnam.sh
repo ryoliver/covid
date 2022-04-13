05b1ef7f72b78f946ae22bc802874a87837e9f9f
Ruth Oliver <ruth.oliver@yale.edu>
2022-04-13 14:19:50 -0700

    compute cbg area

--- src/workflow/workflow_farnam.sh
+++ src/workflow/workflow_farnam.sh
@@ -14,16 +14,18 @@ chmod +x $src/workflow/run_ghm_annotation.sh
 chmod +x $src/workflow/run_event_summary.sh
 chmod +x $src/workflow/run_dbbmm_test.sh
 chmod +x $src/workflow/run_extract_gHM.sh
+chmod +x $src/workflow/run_compute_cbg_area.sh
+
 
 # intersect events with census geometries
-module load R/4.1.0-foss-2020b
-Rscript $src/workflow/create_intersection_joblist.R
+#module load R/4.1.0-foss-2020b
+#Rscript $src/workflow/create_intersection_joblist.R
 
-module load dSQ
-dsq --job-file $src/workflow/joblist.txt --mem-per-cpu 40g -t 2- 
+#module load dSQ
+#dsq --job-file $src/workflow/joblist.txt --mem-per-cpu 40g -t 2- 
 
 # UPDATE WITH DATE
-sbatch dsq-joblist-2022-04-13.sh
+#sbatch dsq-joblist-2022-04-13.sh
 
 # process safegraph data
 #sbatch $src/workflow/run_safegraph_processing.sh
@@ -50,3 +52,6 @@ sbatch dsq-joblist-2022-04-13.sh
 
 # extract gHM from census geometries
 #sbatch $src/workflow/run_extract_gHM.sh
+
+# compute area of census geometries
+sbatch $src/workflow/run_compute_cbg_area.sh
