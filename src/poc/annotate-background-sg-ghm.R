7a5e37ff981757a3de46f94c9206cdec0ab01e5c
Ruth Oliver <ruth.oliver@yale.edu>
2022-05-04 16:16:23 -0700

    change var type

--- src/poc/annotate-background-sg-ghm.R
+++ src/poc/annotate-background-sg-ghm.R
@@ -44,12 +44,12 @@ evt <- data.table::rbindlist(lapply(files, data.table::fread)) %>%
 
 message("reading in census block group geometries...")
 cbg_sf <- st_read(paste0(.wd,"/data/safegraph_open_census_data_2010_to_2019_geometry/cbg.geojson"))
-cbg_area <- fread(paste0(.datPF, "event-annotations/cbg-area.csv"))
+cbg_area <- fread(paste0(.datPF, "event-annotations/cbg-area.csv"), colClasses = "character")
 
 
 message("running intersection with cbg geometries...")
 evt_cbg <- st_intersection(evt,cbg_sf) %>%
-  rename(cbg_2010 = CensusBlockGroup) %>%
+  mutate(cbg_2010 = as.character(CensusBlockGroup)) %>%
   st_drop_geometry() %>%
   left_join(., cbg_area, by = "cbg_2010")
 
