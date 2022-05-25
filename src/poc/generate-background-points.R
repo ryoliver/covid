152e5de080de858607293dedbc8fb5516318b65d
Ruth Oliver <ruth.oliver@yale.edu>
2022-05-25 16:13:18 -0700

    generate background points from centroids

--- src/poc/generate-background-points.R
+++ src/poc/generate-background-points.R
@@ -24,6 +24,8 @@ if(interactive()) {
   
 }
 
+message("start generating background points...")
+
 source(file.path(.wd,'/src/startup.r'))
 
 suppressWarnings(
@@ -33,81 +35,35 @@ suppressWarnings(
     library(amt)
     library(DBI)
     library(RSQLite)
-    library(sf)
   }))
 
-message("connect to db...")
-db <- dbConnect(RSQLite::SQLite(), .dbPF)
+message("read in centroids...")
 
-indtb <- tbl(db,'individual')
-evttb <- tbl(db,'event_clean')
+evt <- fread(paste0(.outPF,"ssf-background-pts/event-daily-centroids.csv"))
 
-message("select test individuals...")
-test <- evttb %>%
-  group_by(individual_id,) %>%
-  summarize(num=n()) %>%
-  arrange(desc(num)) %>%
-  inner_join(indtb, by = 'individual_id') %>%
-  select(taxon_canonical_name, individual_id, num) %>%
-  collect() %>%
-  group_by(taxon_canonical_name) %>%
-  slice(1) %>%
-  ungroup() %>%
-  sample_n(20)
+ids <- unique(e$individual_id)
   
-for(i in 1:nrow(test)){
-  id <- test[i,]$individual_id
+for(i in 1:length(ids)){
+  id <- ids[i]
   
-  e <- evttb %>%
-    filter(individual_id == id) %>%
-    collect() %>%
-    mutate("date_time" = as_datetime(timestamp),
-           "date" = as_date(timestamp),
-           "year" = lubridate::year(date),
-           "doy" = lubridate::yday(date),
-           "lat" = as.numeric(lat),
-           "lon" = as.numeric(lon)) %>%
-    filter(year >= 2019) %>%
-    filter(year <= 2020) %>%
-    filter(doy < 170) 
+  e <- evt %>%
+    filter(individual_id == id) 
   
-  e1 <- e %>%
-    distinct(date, .keep_all = TRUE)
+  tr <- make_track(e, lon, lat, date, 
+                   individual_id = individual_id,
+                   ghm = ghm,
+                   sg_norm = sg_norm)
   
-  tr1 <- make_track(e1, lon, lat, date_time, event_id = event_id)
-  
-  ssf1 <- tr1 %>% 
+  ssf <- tr %>% 
     track_resample(rate = hours(24), tolerance = hours(24)) %>%
     steps_by_burst() %>%
-    random_steps(n_control = 15) %>%
-    mutate("individual_id" = rep(id, nrow(.)))
+    random_steps(n_control = 15) 
   
-  fwrite(ssf1, paste0(.outPF,'ssf-background-pts/individual-files/traditional-approach/individual-',id,".csv"))
-  
-  
-  
-  e2 <- e %>%
-    st_as_sf(., coords = c("lon", "lat")) %>%
-    filter(date == "2020-01-01") %>%
-    dplyr::group_by(date) %>% 
-    summarize(geometry = st_union(geometry)) %>% 
-    st_centroid()
-  
-  
-  tr2 <- make_track(e2, lon, lat, date)
-  
-  ssf2 <- tr2 %>% 
-    track_resample(rate = hours(24), tolerance = hours(24)) %>%
-    steps_by_burst() %>%
-    random_steps(n_control = 15) %>%
-    mutate("individual_id" = rep(id, nrow(.)))
-  
-  fwrite(ssf2, paste0(.outPF,'ssf-background-pts/individual-files/averaging-approach/individual-',id,".csv"))
+  fwrite(ssf, paste0(.outPF,"ssf-background-pts/centroids-background/individual-",id,".csv"))
   
   message(i)
 }
 
-message("done!")
-#ids <- c(1967914051,1967914129,160484522,160484522,578909800)
-
 dbDisconnect(db)
+
+message("done!")
