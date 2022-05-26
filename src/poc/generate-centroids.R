011defbf34476eaf8944801f23e000070c390c89
Ruth Oliver <ruth.oliver@yale.edu>
2022-05-26 10:17:18 -0700

    fix typo

--- src/poc/generate-centroids.R
+++ src/poc/generate-centroids.R
@@ -36,16 +36,20 @@ suppressWarnings(
     library(RSQLite)
   }))
 
+# reading in the grid just to pull the CRS
+# definitely a better way to do this, just keeping for consistency with other BGC projects
 grid <- st_read(paste0(.datPF,"1440x456global_v2_20200527/1440x456global_20200527.shp"))
 
 message("connect to db...")
 db <- dbConnect(RSQLite::SQLite(), .dbPF)
 
 message("read in annotation files...")
+# events annotated with safegraph data
 evt_sg <- fread(paste0(.outPF,"event-annotations/event_sg.csv")) %>%
-  mutate(sg_norm = safegraph_daily_count/cbg_area_m2) %>%
+  mutate(sg_norm = safegraph_daily_count/cbg_area_m2) %>% # normalize device counts by area
   select(event_id, sg_norm)
 
+# events annotated with TNC global human modification
 evt_ghm <- fread(paste0(.outPF,"event-annotations/event_ghm.csv"))
 
 message("create centroids...")
@@ -54,32 +58,34 @@ evt <- dbGetQuery(db,'SELECT event_id,individual_id,lon,lat,timestamp from event
   mutate("date" = as_date(timestamp),
          "year" = lubridate::year(date),
          "doy" = lubridate::yday(date)) %>%
-  filter(year >= 2019) %>%
+  filter(year >= 2019) %>% # lazy check on date filter
   filter(year <= 2020) %>%
   filter(doy < 170) %>%
-  st_as_sf(coords = c("lon", "lat"), crs="+proj=longlat +datum=WGS84") %>%
-  left_join(., evt_sg, by = "event_id") %>%
-  left_join(., evt_ghm, by = "event_id") %>%
-  st_transform(., crs = st_crs(grid)) %>%
-  group_by(individual_id, date) %>% 
+  st_as_sf(coords = c("lon", "lat"), crs="+proj=longlat +datum=WGS84") %>% # convert to sf object
+  left_join(., evt_sg, by = "event_id") %>% # join with safegraph data
+  left_join(., evt_ghm, by = "event_id") %>% # join with modification data
+  st_transform(., crs = st_crs(grid)) %>% # project to equal area projection
+  group_by(individual_id, date) %>%  # collapse into single value per day
   summarize(ghm = mean(ghm, na.rm = TRUE),
             sg_norm = mean(sg_norm, na.rm = TRUE),
             geometry = st_union(geometry)) %>%
   st_centroid() %>%
-  st_transform(., crs="+proj=longlat +datum=WGS84") 
+  st_transform(., crs="+proj=longlat +datum=WGS84") # transform back
 
 message("extract coordinates...")
-coords <- st_coordinates(evt)
+coords <- st_coordinates(evt) # extract lat/long coordinates
 
+# append coords to event table
 evt <- cbind(evt,coords)
 
+# convert to data frame to write out
 evt <- evt %>%
   st_drop_geometry() %>%
   rename(lon = X,
          lat = Y)
 
 message("write out centroid shp file...")
-st_write(e, paste0(.outPF,"ssf-background-pts/event-daily-centroids.csv"))
+st_write(evt, paste0(.outPF,"ssf-background-pts/event-daily-centroids.csv"))
   
 dbDisconnect(db)
   
