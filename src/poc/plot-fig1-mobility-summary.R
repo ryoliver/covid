4b75f8cf5230ee62b5b96e7b3cab0c28166fb508
Ruth Oliver <ruth.oliver@yale.edu>
2022-08-29 16:03:27 -0700

    create 2d density plot

--- src/poc/plot-fig1-mobility-summary.R
+++ src/poc/plot-fig1-mobility-summary.R
@@ -17,7 +17,6 @@ if(interactive()) {
   .script <-  thisfile()
   rd <- is_rstudio_project$make_fix_file(.script)
   
-  .dbPF <- '/gpfs/loomis/pi/jetz/sy522/covid-19_movement/processed_data/mosey_mod.db'
   .datPF <- file.path(.wd,'data/')
   .outPF <- file.path(.wd,'analysis/')
 }
@@ -28,39 +27,62 @@ suppressWarnings(
   suppressPackageStartupMessages({
     library(data.table)
     library(cowplot)
+    library(lubridate)
   }))
 
-d <- fread(paste0(.outPF,"dbbmm_size.csv")) %>%
-  mutate(sg_norm = sg/cbg_area)
 
-ggplot(data = subset(d, sg_norm < 0.0000002))+
-  geom_histogram(aes(x = sg_norm, y = ..density.., 
-                     group = as.factor(year), fill = as.factor(year)),
-                 alpha = 0.5, position = "identity") +
+d <- fread(paste0(.outPF,"events.csv"))
+
+d <- d %>%
+  filter(!is.na(safegraph_daily_count)) %>%
+  filter(!is.na(ghm)) %>%
+  mutate(year = lubridate::year(timestamp),
+         sg_norm = safegraph_daily_count/cbg_area_m2) %>%
+  select(individual_id, timestamp, year, sg_norm, ghm) %>%
+  distinct(individual_id, timestamp, .keep_all = TRUE)
+
+
+
+
+p1 <- ggplot(data = subset(d, year == 2019),
+             aes(y = sg_norm, x = ghm)) +
+  geom_bin2d(bins = 70) +
+  scale_fill_continuous(type = "viridis", trans = "log10") +
   theme_cowplot() +
-  theme(legend.position = c(0.8, 0.2),
+  theme(legend.position = "none",
         legend.title = element_blank(),
-        axis.title.y = element_text(size = 9),
-        axis.title.x = element_blank(),
+        axis.title = element_text(size = 9),
         axis.text = element_text(size = 8),
         axis.text.x = element_text(size = 9),
         legend.text = element_text(size = 8)) +
   scale_x_continuous(expand = expansion(mult = c(0, 0.01))) +
-  scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
-  labs(y = "Density") 
+  scale_y_continuous(expand = expansion(mult = c(0, 0.01)), 
+                     limits = c(NA, 0.004),
+                     labels = comma) +
+  labs(x = "modification", y = "mobility")
 
-ggplot(data = subset(d, sg_norm < 0.0000002))+
-  geom_density(aes(x = sg_norm, y = ..density.., 
-                     group = as.factor(year), color = as.factor(year)),
-                 alpha = 0.5, position = "identity") +
+
+p2 <- ggplot(data = subset(d, year == 2020),
+             aes(y = sg_norm, x = ghm)) +
+  geom_bin2d(bins = 70) +
+  scale_fill_continuous(type = "viridis", trans = "log10") +
   theme_cowplot() +
-  theme(legend.position = c(0.8, 0.2),
+  theme(legend.position = c(0.1, 0.8),
         legend.title = element_blank(),
-        axis.title.y = element_text(size = 9),
-        axis.title.x = element_blank(),
+        axis.title = element_text(size = 9),
         axis.text = element_text(size = 8),
         axis.text.x = element_text(size = 9),
         legend.text = element_text(size = 8)) +
   scale_x_continuous(expand = expansion(mult = c(0, 0.01))) +
-  scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
-  labs(y = "Density") 
+  scale_y_continuous(expand = expansion(mult = c(0, 0.01)), 
+                     limits = c(NA, 0.004),
+                     labels = comma) +
+  labs(x = "modification", y = "mobility")
+
+i <- 1/2
+pdf("~/Desktop/test.pdf", width = 12, height = 5)
+ggdraw() +
+  draw_plot(p1, x = 0, y = 0, width = i, height = 1) +
+  draw_plot(p2, x = i, y = 0, width = i, height = 1)
+
+dev.off()
