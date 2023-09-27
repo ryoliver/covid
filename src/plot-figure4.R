6c3f44a7954ab936c2f473612ca682e221150020
Ruth Oliver <rutholiver@Ruths-MacBook-Pro.local>
2023-09-27 16:32:24 -0700

    try geomdots

--- src/plot-figure4.R
+++ src/plot-figure4.R
@@ -12,9 +12,7 @@ library(ggbeeswarm)
 
 rm(list = ls())
 
-#5D5D81
-
-pred_dat <- read_csv("~/Desktop/covid-results/area_change_prediction_2023-06-23.csv")
+pred_dat <- fread("~/Desktop/covid-results/area_change_prediction_2023-09-27.csv")
 
 spl <- unique(pred_dat$species)
 
@@ -52,20 +50,31 @@ area_diff_df <- do.call("rbind", diff_out) %>%
   mutate(direct = case_when(diff_km > 0 ~ "p",
                             diff_km < 0 ~ "n"),
          group = rep("a",nrow(.))) %>%
-  mutate(taxa = case_when(species %in% c("Anser caerulescens",
+  mutate(taxa = case_when(species %in% c("Anas acuta",
+                                                 "Anas americana",
+                                                 "Anas clypeata",
+                                                 "Anas crecca",
+                                                 "Anas cyanoptera",
+                                                 "Anas platyrhynchos",
+                                                 "Anas strepera",
+                                                 "Anser albifrons",
+                                                 "Anser caerulescens",
+                                                 "Aquila chrysaetos",
                                                  "Ardea alba",
                                                  "Aquila chrysaetos",
+                                                 "Chen rossii",
+                                                 "Circus cyaneus",
                                                  "Corvus corax",
-                                                 "Haliaeetus leucocephalus") ~ "birds",
+                                                 "Haliaeetus leucocephalus",
+                                                 "Rallus longirostris") ~ "birds",
                           species %in% c("Alces alces",
                                                  "Antilocapra americana",
+                                                 "Canis latrans",
                                                  "Cervus elaphus",
+                                                 "Lynx rufus",
                                                  "Odocoileus hemionus",
                                                  "Odocoileus virginianus",
                                                  "Ovis canadensis",
-                                                 "Canis latrans",
-                                                 "Canis lupus",
-                                                 "Lynx rufus",
                                                  "Puma concolor",
                                                  "Ursus americanus",
                                                  "Ursus arctos") ~ "mammals")) 
@@ -78,7 +87,7 @@ area_mean_diff <- area_diff_df %>%
 
 
 
-pred_dat <- read_csv("~/Desktop/covid-results/niche_change_prediction_2023-07-11.csv")
+pred_dat <- fread("~/Desktop/covid-results/niche_change_prediction_2023-09-27.csv")
 
 spl <- unique(pred_dat$species)
 
@@ -113,26 +122,34 @@ niche_diff_df <- do.call("rbind", diff_out) %>%
   mutate(direct = case_when(diff > 0 ~ "p",
                             diff < 0 ~ "n"),
          group = rep("a",nrow(.))) %>%
-  mutate(taxa = case_when(species %in% c("Anser caerulescens",
+  mutate(taxa = case_when(species %in% c("Anas acuta",
+                                         "Anas americana",
+                                         "Anas clypeata",
+                                         "Anas crecca",
+                                         "Anas cyanoptera",
+                                         "Anas platyrhynchos",
+                                         "Anas strepera",
+                                         "Anser albifrons",
+                                         "Anser caerulescens",
+                                         "Aquila chrysaetos",
                                          "Ardea alba",
                                          "Aquila chrysaetos",
+                                         "Chen rossii",
+                                         "Circus cyaneus",
                                          "Corvus corax",
                                          "Haliaeetus leucocephalus",
-                                         "Grus canadensis",
-                                         "Numenius americanus") ~ "birds",
+                                         "Rallus longirostris") ~ "birds",
                           species %in% c("Alces alces",
                                          "Antilocapra americana",
+                                         "Canis latrans",
                                          "Cervus elaphus",
+                                         "Lynx rufus",
                                          "Odocoileus hemionus",
                                          "Odocoileus virginianus",
                                          "Ovis canadensis",
-                                         "Canis latrans",
-                                         "Canis lupus",
-                                         "Lynx rufus",
                                          "Puma concolor",
                                          "Ursus americanus",
-                                         "Ursus arctos",
-                                         "Sus scrofa") ~ "mammals")) 
+                                         "Ursus arctos") ~ "mammals")) 
 
 
 niche_mean_diff <- niche_diff_df %>% 
@@ -140,13 +157,17 @@ niche_mean_diff <- niche_diff_df %>%
   pull(mdiff) %>% 
   round(1)
 
+area_diff_df <- area_diff_df %>%
+  filter(species != "Haliaeetus leucocephalus")
+
 max_val <- max(c(min(area_diff_df$diff_km, na.rm = TRUE), max(area_diff_df$diff_km, na.rm = TRUE)))
 
 
-p1 <- ggplot(area_diff_df, aes(x = -diff_km, y = group, color = taxa)) +
+p1 <- ggplot(area_diff_df, 
+             aes(x = -diff_km, y = group, color = taxa)) +
   geom_vline(aes(xintercept = 0), linetype = "solid", alpha = 0.5, color = "#4a4e4d") +
   geom_vline(aes(xintercept = -area_mean_diff), linetype = "dotted", color = "#4a4e4d") +
-  geom_beeswarm(cex = 8, alpha = 0.7, size = 2) +
+  geom_beeswarm(cex = 8, alpha = 0.7, size = 1) +
   geom_point(aes(x = -area_mean_diff), size = 3, shape = 22, 
              fill = "#ABB0AE", color = "#4A4E4D") +
   scale_color_manual(values = c("#FF9B54","#A7D3A6")) +
@@ -171,7 +192,7 @@ max_val <- max(c(min(niche_diff_df$percent_change, na.rm = TRUE), max(niche_diff
 p2 <- ggplot(niche_diff_df, aes(x = percent_change, y = group, color = taxa)) +
   geom_vline(aes(xintercept = 0), linetype = "solid", alpha = 0.5, color = "#4a4e4d") +
   geom_vline(aes(xintercept = niche_mean_diff), linetype = "dotted", color = "#4a4e4d") +
-  geom_beeswarm(cex = 8, alpha = 0.7, size = 2) +
+  geom_beeswarm(cex = 8, alpha = 0.7, size = 1) +
   geom_point(aes(x = niche_mean_diff), size = 3, shape = 22, 
              fill = "#ABB0AE", color = "#4A4E4D") +
   scale_color_manual(values = c("#FF9B54","#A7D3A6")) +
@@ -193,3 +214,27 @@ p2 <- ggplot(niche_diff_df, aes(x = percent_change, y = group, color = taxa)) +
 p <- p1/p2
 
 ggsave(p, file = "~/Desktop/figure4.png", width = 3.5, height = 2)
+
+p1 <- ggplot(area_diff_df, aes(x = -diff_km, fill = taxa)) +
+  geom_dotplot(binwidth = 0.5,stackdir = "center", alpha = 0.7) +
+  scale_fill_manual(values = c("#FF9B54","#A7D3A6")) +
+  scale_y_continuous(NULL, breaks = NULL) +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.line.x = element_line(colour = "#4a4e4d", linewidth =0.3, linetype='solid'),
+        legend.position = "none",
+        axis.text.y = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_text(size = 6),
+        axis.title.x = element_text(size = 6, 
+                                    face = "bold"),
+        axis.ticks.x = element_line(color = "#4a4e4d")) +
+  labs(x = bquote(bold('Change in area size'~(km^2))))
+
+p <- p1/p2
+
+ggsave(p, file = "~/Desktop/figure4.png", width = 3.5, height = 2)
+
+
